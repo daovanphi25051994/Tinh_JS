3987915bc2d320756adefc4772b2ab45e7ed29aa
phi <daovanphi25051994@gmail.com>
2020-04-05 00:21:44 +0700

    ok

--- app.js
+++ app.js
@@ -1,41 +1,80 @@
-function plus(num1, num2) {
-    return num1 + num2;
-}
-
-function minus(num1, num2) {
-    return num1 - num2
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
-}
-
-function showResult() {
-    let valueNum1 = parseInt(document.getElementById("num1").value);
-    let valueNum2 = parseInt(document.getElementById("num2").value);
-    let choose = document.getElementById("choose").value;
-    switch (choose) {
-        case "+" :
-            let result1 = plus(valueNum1, valueNum2);
-            document.getElementById("result").innerHTML = "result is " + result1;
-            break;
-        case "-" :
-            let result2 = minus(valueNum1, valueNum2);
-            document.getElementById("result").innerHTML = "result is " + result2;
-            break;
-        case "*" :
-            let result3 = multiply(valueNum1, valueNum2);
-            document.getElementById("result").innerHTML = "result is " + result3;
-            break;
-        case "/" :
-            let result4 = divide(valueNum1, valueNum2);
-            document.getElementById("result").innerHTML = "result is " + result4;
-            break;
+function getInfo() {
+    let mssv = document.getElementById("msSinhVien").value;
+    console.log(mssv);
+    if (mssv == "") {
+        document.getElementById("error-mssv").innerHTML = "*Vui lòng nhập mssv";
+        return;
     }
-    document.getElementById("num1").value = "";
-    document.getElementById("num2").value = "";
+
+    let hoTen = document.forms["info"].hoTen.value;
+    if (hoTen == "") {
+        document.getElementById("error-hoTen").innerHTML = "*Vui lòng nhập Họ Tên";
+        return ;
+    }
+    let email = document.forms["info"].email.value;
+    if (email == "") {
+        document.getElementById("error-email").innerHTML = "*Vui lòng nhập Email";
+        return ;
+    }
+
+    if (isNaN(email) == "true") {
+        document.getElementById("error-email").innerHTML =
+            "*Vui lòng nhập đúng định dạng Email";
+        return ;
+    }
+    let gioiTinh = document.forms["info"].gioiTinh.value;
+    if (gioiTinh == "") {
+        document.getElementById("error-gioiTinh").innerHTML =
+            "*Vui lòng chọn giới tính";
+        return ;
+    }
+    let soThich = document.getElementsByName('soThich');
+    let danhSachSoThich = "";
+    for (var i = 0; i < soThich.length; i++) {
+        if (soThich[i].checked === true) {
+            danhSachSoThich += soThich[i].value + " , ";
+        }
+    }
+    if (danhSachSoThich == "") {
+        document.getElementById("error-soThich").innerHTML =
+            "*Vui lòng chọn sở thích";
+        return ;
+    }
+    let quocTich = document.forms["info"].quocTich.value;
+    if (quocTich == "") {
+        document.getElementById("error-quocTich").innerHTML =
+            "*Vui lòng chọn quốc tịch";
+        return ;
+    }
+    let ghiChu = document.forms["info"].ghiChu.value;
+    if (ghiChu == "") {
+        alert("ghi chú:Ít hơn 200 kí tự");
+        return ;
+    }
+    var text = document.getElementById("msSinhVien");
+    if (text.value == "") {
+        text.style.backgroundColor = "#d0eb18";
+    }
+    var text = document.getElementById("hoTen");
+    if (text.value == "") {
+        text.style.backgroundColor = "#d0eb18";
+    }
+    var text = document.getElementById("email");
+    if (text.value == "") {
+        text.style.backgroundColor = "#d0eb18";
+        return false;
+    }
+    let info = "";
+    info = "mssv:" + mssv + "<br>";
+    info += "họ và tên :" + hoTen + "<br>";
+    info += "email :" + email + "<br>";
+    info += "Giới tính :" + gioiTinh + "<br>";
+    info += "Sở thích :" + danhSachSoThich + "<br>";
+    info += "Quốc tịch :" + quocTich + "<br>";
+    info += "Ghi chú :" + ghiChu + "<br>";
+    document.getElementById("output").innerHTML = info;
 }
+
+
+
+
